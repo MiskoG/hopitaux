2bbc77be76c736394007f57c4b9b09304d472f21
Misko <mgiboreau@gmail.com>
2016-03-09 17:01:17 +0100

    Add markers icons

--- js/map.js
+++ js/map.js
@@ -10,14 +10,18 @@ function initMap() {
   // Let's initialize the hospitals data
   var hospitals = JSON.parse(hospitalsJSON);
 
+  var imageMarkers = ['./img/marker-none.png','./img/marker-1.png','./img/marker-2.png','./img/marker-3.png'];
+
   for (var i = 0; i < hospitals.length ; i++) {
+  	var j = i%4;
   	// Create a marker and set its position.
 	var marker = new google.maps.Marker({
 		map: map,
 		position: 
 			{lat: parseFloat(hospitals[i].latitude), 
 			lng: parseFloat(hospitals[i].longitude)},
-		title: hospitals[i].name
+		title: hospitals[i].name,
+		icon: imageMarkers[j]
 	});
   };
 }
