395d0110192fa37fb8070336dfa3b2271af81ed9
Misko <mgiboreau@gmail.com>
2016-03-09 16:30:31 +0100

    Add hospitals location on the map

--- js/map.js
+++ js/map.js
@@ -1,5 +1,4 @@
 function initMap() {
-  var myLatLng = {lat: 48.858, lng: 2.32};
 
   // Create a map object and specify the DOM element for display.
   var map = new google.maps.Map(document.getElementById('map'), {
@@ -8,10 +7,17 @@ function initMap() {
     zoom: 13
   });
 
-  // Create a marker and set its position.
-  var marker = new google.maps.Marker({
-    map: map,
-    position: myLatLng,
-    title: 'Hello World!'
-  });
+  // Let's initialize the hospitals data
+  var hospitals = JSON.parse(hospitalsJSON);
+
+  for (var i = 0; i < hospitals.length ; i++) {
+  	// Create a marker and set its position.
+	var marker = new google.maps.Marker({
+		map: map,
+		position: 
+			{lat: parseFloat(hospitals[i].latitude), 
+			lng: parseFloat(hospitals[i].longitude)},
+		title: hospitals[i].name
+	});
+  };
 }
