83627e46b38548500edb7db3bc8a1fa74a2239ef
Misko <mgiboreau@gmail.com>
2016-03-13 10:29:35 +0100

    Add menu links and depedency on the markers

--- js/map.js
+++ js/map.js
@@ -1,27 +1,68 @@
-function initMap() {
+var map;
+var markers = [];
+var imageMarkers = ['./img/marker-none.png','./img/marker-1.png','./img/marker-2.png','./img/marker-3.png'];
+var hospitals = JSON.parse(hospitalsJSON);
 
+function initMap() {
   // Create a map object and specify the DOM element for display.
-  var map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 48.858, lng: 2.32},
     scrollwheel: false,
     zoom: 12
   });
 
-  // Let's initialize the hospitals data
-  var hospitals = JSON.parse(hospitalsJSON);
+  getMarkersGeneral();
+}
 
-  var imageMarkers = ['./img/marker-none.png','./img/marker-1.png','./img/marker-2.png','./img/marker-3.png'];
+// Gets and displays the markers for the GENERAL category
+function getMarkersGeneral() {
+  deleteMarkers();
+  getMarkersByCategory('general');
+}
 
+// Gets and displays the markers for the URGENCES category
+function getMarkersUrgences() {
+  deleteMarkers();
+  getMarkersByCategory('urgences');
+}
+
+// Gets the list of markers depending on its category : General, Urgences, Maternité or Séjours longs
+function getMarkersByCategory(category) {
+  if(category == 'general') {
+    imageMarker = 2;
+  }
+  else {
+    imageMarker = 1
+  }
   for (var i = 0; i < hospitals.length ; i++) {
-  	var j = i%4;
-  	// Create a marker and set its position.
-	var marker = new google.maps.Marker({
-		map: map,
-		position: 
-			{lat: parseFloat(hospitals[i].latitude), 
-			lng: parseFloat(hospitals[i].longitude)},
-		title: hospitals[i].name,
-		icon: imageMarkers[j]
-	});
+    var latitude = parseFloat(hospitals[i].latitude);
+    var longitude = parseFloat(hospitals[i].longitude);
+    addMarker(latitude,longitude,hospitals[i].name,imageMarker);
   };
+}
+
+// Adds a marker to the map and push to the array.
+function addMarker(latitude,longitude,name,icon) {
+  var marker = new google.maps.Marker({
+    map: map,
+    position: 
+      {lat: latitude, 
+      lng: longitude},
+    title: name,
+    icon: imageMarkers[icon]
+  });
+  markers.push(marker);
+}
+
+// Sets the map on all markers in the array.
+function setMapOnAll(map) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+}
+
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers() {
+  setMapOnAll(null);
+  markers = [];
 }
